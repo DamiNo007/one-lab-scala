f4d437fbe2ee9bdac18971c5c6ebd97ec4ca341f
DamiNo007 <dmoldabayev.99@gmail.com>
2020-07-11 23:20:52 +0600

    HomeWork3 Collections and Githuber done

--- src/main/scala/one/lab/tasks/week/three/Collections.scala
+++ src/main/scala/one/lab/tasks/week/three/Collections.scala
@@ -1,18 +1,90 @@
 package one.lab.tasks.week.three
 
+import one.lab.demos.week.two.Demos.Empty
+import one.lab.tasks.week.one.Recursion.printNTimes
+
+import scala.annotation.tailrec
+
 object Collections {
 
   // duplicateNTimes(3, List(1,2,3)) == List(1,1,1,2,2,2,3,3,3)
   // duplicateNTimes(3, List()) == List()
-  def duplicateNTimes[A](n: Int, list: List[A]): List[A] = ???
+  def duplicateNTimes[A](n: Int, list: List[A]): List[A] = {
+    @tailrec
+    def dup(list: List[A], dupList: List[A]): List[A] = list match {
+      case Nil => throw new NoSuchElementException()
+      case head :: tail => if (tail.isEmpty) dupList ::: makeN(n, head) else dup(tail, dupList ::: makeN(n, head))
+    }
+
+    def makeN(n: Int, num: A): List[A] = {
+      @tailrec
+      def nTimes(m: Int, dupList: List[A]): List[A] = m match {
+        case 0 => dupList
+        case _ => nTimes(m - 1, num :: dupList)
+      }
+
+      nTimes(n - 1, List(num))
+    }
+
+    dup(list, List())
+  }
+
+  //Implemented with a flatmap here
+  def duplicateNTimesUsingFlatMap[A](n: Int, list: List[A]): List[A] = {
+    def makeN(n: Int, num: A): List[A] = {
+      @tailrec
+      def nTimes(m: Int, dupList: List[A]): List[A] = m match {
+        case 0 => dupList
+        case _ => nTimes(m - 1, num :: dupList)
+      }
+
+      nTimes(n - 1, List(num))
+    }
+
+    list flatMap { l => makeN(n, l) }
+  }
 
   // splitAtK(4, List(1,2,3,4,5,6,7,8,9)) == (List(1,2,3,4), List(5,6,7,8,9))
   // splitAtK(0, List(1,2,3)) == (List(), List(1,2,3))
-  def splitAtK[A](k: Int, list: List[A]): (List[A], List[A]) = ???
+  def splitAtK[A](k: Int, list: List[A]): (List[A], List[A]) = {
+    @tailrec
+    def split(n: Int, list: List[A], remainder: List[A]): (List[A], List[A]) = (n, list) match {
+      case (_, Nil) => (remainder.reverse, Nil)
+      case (0, l) => (remainder.reverse, l)
+      case (k, head :: tail) => split(k - 1, tail, head :: remainder)
+    }
+
+    split(k, list, Nil)
+  }
 
   // removeKthElement(5, List(1,2,3,4,5,6)) == (List(1,2,3,4,5), 6)
-  // removeKthElement(2, List(1,2,3,4,5,6)) == (List(1,2,4,5,6), 2)
+  // removeKthElement(2, List(1,2,3,4,5,6)) == (List(1,2,4,5,6), 3)
   // removeKthElement(-3, List(1,2,3,4,5,6)) == IndexOutOfBoundException
   // removeKthElement(1000, List(1,2,3,4,5,6)) == IndexOutOfBoundException
-  def removeKthElement[A](k: Int, list: List[A]): (List[A], A) = ???
+
+  def removeKthElement[A](k: Int, list: List[A]): (List[A], A) = {
+    if (k < 0) throw new IndexOutOfBoundsException
+
+    @tailrec
+    def remove(list: List[A], beforeK: List[A], i: Int, k: Int): (List[A], A) = list match {
+      case Nil => throw new IndexOutOfBoundsException
+      case x :: tail => if (i == k) ((beforeK.reverse ::: tail), x) else remove(tail, (x :: beforeK), i + 1, k)
+    }
+
+    remove(list, List(), 0, k)
+  }
+
+  def main(args: Array[String]): Unit = {
+    println("Checking duplicate methods")
+    println(duplicateNTimes(3, List(1, 2, 3)) == List(1, 1, 1, 2, 2, 2, 3, 3, 3))
+    println(duplicateNTimesUsingFlatMap(3, List()) == List())
+    println("Checking split method")
+    println(splitAtK(4, List(1, 2, 3, 4, 5, 6, 7, 8, 9)) == (List(1, 2, 3, 4), List(5, 6, 7, 8, 9)))
+    println(splitAtK(0, List(1, 2, 3)) == (List(), List(1, 2, 3)))
+    println("Checking remove method")
+    println(removeKthElement(5, List(1, 2, 3, 4, 5, 6)) == (List(1, 2, 3, 4, 5), 6))
+    println(removeKthElement(2, List(1, 2, 3, 4, 5, 6)) == (List(1, 2, 4, 5, 6), 3))
+    println(removeKthElement(-3, List(1, 2, 3, 4, 5, 6)))
+    println(removeKthElement(1000, List(1, 2, 3, 4, 5, 6)))
+  }
 }
--- src/main/scala/one/lab/tasks/week/three/Githuber.scala
+++ src/main/scala/one/lab/tasks/week/three/Githuber.scala
@@ -8,26 +8,53 @@ import scala.concurrent.ExecutionContextExecutor
 import scala.concurrent.Future
 import org.json4s.DefaultFormats
 import org.json4s.jackson.JsonMethods.parse
-
+import RestClientImpl._
 import scala.util.Failure
 import scala.util.Success
 
 object Githuber extends App {
-  implicit val system: ActorSystem                        = ActorSystem("lalka")
-  implicit val materializer: ActorMaterializer            = ActorMaterializer.create(system)
+  implicit val system: ActorSystem = ActorSystem("lalka")
+  implicit val materializer: ActorMaterializer = ActorMaterializer.create(system)
   implicit val executionContext: ExecutionContextExecutor = ExecutionContext.global
-  implicit val defaultFormats: DefaultFormats.type        = DefaultFormats
+  implicit val defaultFormats: DefaultFormats.type = DefaultFormats
 
   // TODO: поля можете добавить какие хотите
-  case class GithubUser()
-  case class GithubRepository()
+  case class GithubUser(login: String, name: String, avatarUrl: Option[String], public_repos: Option[String])
+
+  case class GithubRepository(name: String,
+                              size: Int,
+                              fork: Boolean,
+                              pushed_at: String,
+                              stargazers_count: Int)
 
   //  https://api.github.com/users/{$USER}
-  def getGithubUser(username: String): Future[GithubUser] = ???
+  def getGithubUser(username: String): Future[GithubUser] = {
+    get(s"https://api.github.com/users/$username")
+      .map {
+        body => parse(body).extract[GithubUser]
+      }
+  }
 
-  def getUserRepositories(repoUrl: String): Future[List[GithubRepository]] = ???
+  def getUserRepositories(repoUrl: String): Future[List[GithubRepository]] = {
+    get(repoUrl)
+      .map {
+        body => parse(body).extract[List[GithubRepository]]
+      }
+  }
 
-  def getUserInfo(username: String): Unit = ???
+  def getUserInfo(username: String): Unit = {
+    getGithubUser(username)
+      .flatMap(user => {
+        println(s"${user.name} called ${user.login} under avatar ${user.avatarUrl.getOrElse("None")} has ${user.public_repos.getOrElse("None")} repositories in his github account: \n")
+        getUserRepositories(s"https://api.github.com/users/${user.login}/repos")
+      })
+      .onComplete {
+        case Success(responses) =>
+          for (response <- responses)
+            println(s"${response.name}: size = ${response.size}, stargazers = ${response.stargazers_count}, push date = ${response.pushed_at}, fork = ${if (response.fork) "Forked" else "Not Forked"}")
+        case Failure(e) => println(e.getMessage)
+      }
+  }
 
-  getUserInfo("")
+  getUserInfo("DamiNo007")
 }
